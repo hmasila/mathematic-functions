5fc3c1253998694d6d5f195102a2135ca049b85b
Hannah Masila <hannahmasila@gmail.com>
2020-01-10 17:39:21 +0200

    round to two

--- app/controllers/arithmetics_controller.rb
+++ app/controllers/arithmetics_controller.rb
@@ -3,7 +3,7 @@ class ArithmeticsController < ApplicationController
 		arr = params[:numbers]
 		ans = arr.inject{ |sum, el| sum + el }.to_f / arr.size
 
-		render json: ans, status: 200
+		render json: ans.round(2), status: 200
 	rescue
 		render json: {"message": "An error occurred"}, status: 200
 	end
