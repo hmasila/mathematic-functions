f4c97df4754d6ec26c4d919c4cfd513784baeac8
Hannah Masila <hannahmasila@gmail.com>
2020-01-10 16:21:02 +0200

    comment out stuff

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  # config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
@@ -34,13 +34,13 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
-  config.active_support.deprecation = :log
+  # config.active_support.deprecation = :log
 
   # Raise an error on page load if there are pending migrations.
   # config.active_record.migration_error = :page_load
 
   # Highlight code that triggered database queries in logs.
-  config.active_record.verbose_query_logs = true
+  # config.active_record.verbose_query_logs = true
 
 
   # Raises error for missing translations.
