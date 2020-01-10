b17a2236c99ae816d9e577e52f50e27b1814c412
Hannah Masila <hannahmasila@gmail.com>
2020-01-10 16:19:03 +0200

    return active storage

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,11 @@ require "rails"
 # Pick the frameworks you want:
 require "active_storage/engine"
 require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_mailbox/engine"
+require "action_text/engine"
+require "action_view/railtie"
+require "action_cable/engine"
 # require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
